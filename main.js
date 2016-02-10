e2a26cab416cbfcfd0d0e25b692694f58b524d81
Interbred Monkey <interbred.monkey@gmail.com>
2016-02-10 15:01:55 +0000

    Added the logger to the exports so that it can be used

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 module.exports = {
   geocoder: require('./includes/geocoder.js'),
-  ncrypt: require('./includes/ncrypt.js')
+  ncrypt: require('./includes/ncrypt.js'),
+  logger: require('./includes/logger.js')
 }
