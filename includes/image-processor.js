d5c3cc2dae583f9d839bdd029cf18de6eba6eb3d
Matt Collins <matt@scoot.co.uk>
2016-04-12 16:21:10 +0100

    image processor wrapper

---
+++ includes/image-processor.js
@@ -0,0 +1,20 @@
+// Wrapper around the Image Processor microservice
+
+module.exports = function(method, endpoint, opts, callback) {
+  
+  var _ 			= require('underscore');
+  var config 	= require('./config.js')('image-processor');
+  var curl 		= require('./curl.js');
+
+  // 1. Make a request to 'endpoint' on config.host
+  //    This should be done using a common curl library
+
+  // 2. Return the response
+
+  curl(method, config.host + endpoint, opts, function(err, data) {
+
+    return callback(err, data);
+
+  })
+
+}
--- main.js
+++ main.js
@@ -2,5 +2,6 @@
 module.exports = {
   geocoder: require('./includes/geocoder.js'),
   ncrypt: require('./includes/ncrypt.js'),
-  logger: require('./includes/logger.js')
+  logger: require('./includes/logger.js'),
+  images: require('./includes/image-processor.js')
 }
